e646e9d9971cdd6f3f3786a3f42c7de632196d5e
1960067872 <1960067872@qq.com>
2020-02-29 14:27:03 +0800

    1

--- init/util/db.js
+++ init/util/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const pool = mysql.createPool({
-	host: '47.98.244.122',
-	user: 'root',
-	password: 'root654321',
-	database: 'bimiben'
+	host: '',
+	user: '',
+	password: '',
+	database: ''
 })
 
 let query = function(sql, values) {
